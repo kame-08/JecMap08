d25b2d2638e1cc1638dc7c96061e151d761a99a6
Kame2021 <21cm0108@jec.ac.jp>
2022-09-06 09:27:44 +0900

    Initial Commit

---
+++ JecMap/JecMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  JecMapApp.swift
+//  JecMap
+//
+//  Created by Ryo on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct JecMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
