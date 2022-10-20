74b56ee02bc9fddbe78c1f5f171f4ac5831cbc5a
ryo <21cm0108@jec.ac.jp>
2022-10-20 12:00:22 +0900

    Fix:本館、3号館、7号館の緯度経度を建物の中心から入り口に変更

--- JecMap/ContentViewModel.swift
+++ JecMap/ContentViewModel.swift
@@ -12,13 +12,16 @@ class ContentViewModel:ObservableObject {
     
     
     let jec: [Jec] = [
-        Jec(nun: 1, name: "本館", location: CLLocationCoordinate2D(latitude: 35.69857244753979, longitude: 139.6981410560849), aed: true),
+        //入口
+        Jec(nun: 1, name: "本館", location: CLLocationCoordinate2D(latitude: 35.69820, longitude: 139.69810), aed: true),
         Jec(nun: 2, name: "2号館", location: CLLocationCoordinate2D(latitude: 35.6989985881511, longitude: 139.69796247142756), aed: false),
-        Jec(nun: 3, name: "3号館", location: CLLocationCoordinate2D(latitude: 35.69868166322867, longitude: 139.69839971375617), aed: false),
+        //入口
+        Jec(nun: 3, name: "3号館", location: CLLocationCoordinate2D(latitude: 35.69850, longitude: 139.69830), aed: false),
         Jec(nun: 4, name: "4号館", location: CLLocationCoordinate2D(latitude: 35.698926436943914, longitude: 139.69819260026327), aed: false),
         Jec(nun: 5, name: "5号館", location: CLLocationCoordinate2D(latitude: 35.6990939365779, longitude: 139.69743022724919), aed: false),
         Jec(nun: 6, name: "6号館", location: CLLocationCoordinate2D(latitude: 35.69842508965526, longitude: 139.69796247142745), aed: false),
-        Jec(nun: 7, name: "7号館", location: CLLocationCoordinate2D(latitude: 35.69894094701618, longitude: 139.69653272724912), aed: true),
+        //入口
+        Jec(nun: 7, name: "7号館", location: CLLocationCoordinate2D(latitude: 35.69890, longitude: 139.69681), aed: true),
         Jec(nun: 8, name: "8号館", location: CLLocationCoordinate2D(latitude: 35.69761045281154, longitude: 139.69779081005694), aed: true),
         Jec(nun: 9, name: "9号館", location: CLLocationCoordinate2D(latitude: 35.69938827675634, longitude: 139.69801154259176), aed: true),
         Jec(nun: 10, name: "10号館", location: CLLocationCoordinate2D(latitude: 35.69955123784045, longitude: 139.6979809137563), aed: false),
