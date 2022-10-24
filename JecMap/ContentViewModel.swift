fe57957a70aefc95a917e95d24dc35eeeec3f5cf
Kame2021 <21cm0108@jec.ac.jp>
2022-10-24 23:37:57 +0900

    Update:3号館、4号館、5号館、7号館、8号館、9号館、12号館の緯度経度を建物の中心から入り口に変更

--- JecMap/ContentViewModel.swift
+++ JecMap/ContentViewModel.swift
@@ -15,17 +15,22 @@ class ContentViewModel:ObservableObject {
         Jec(nun: 1, name: "本館", location: CLLocationCoordinate2D(latitude: 35.69820, longitude: 139.69810), aed: true),
         Jec(nun: 2, name: "2号館", location: CLLocationCoordinate2D(latitude: 35.6989985881511, longitude: 139.69796247142756), aed: false),
         //入口
-        Jec(nun: 3, name: "3号館", location: CLLocationCoordinate2D(latitude: 35.69850, longitude: 139.69830), aed: false),
-        Jec(nun: 4, name: "4号館", location: CLLocationCoordinate2D(latitude: 35.698926436943914, longitude: 139.69819260026327), aed: false),
-        Jec(nun: 5, name: "5号館", location: CLLocationCoordinate2D(latitude: 35.6990939365779, longitude: 139.69743022724919), aed: false),
+        Jec(nun: 3, name: "3号館", location: CLLocationCoordinate2D(latitude: 35.69850, longitude: 139.69828), aed: false),
+        //入口
+        Jec(nun: 4, name: "4号館", location: CLLocationCoordinate2D(latitude: 35.69875, longitude: 139.69820), aed: false),
+        //入口
+        Jec(nun: 5, name: "5号館", location: CLLocationCoordinate2D(latitude: 35.69892, longitude: 139.69750), aed: false),
         Jec(nun: 6, name: "6号館", location: CLLocationCoordinate2D(latitude: 35.69842508965526, longitude: 139.69796247142745), aed: false),
         //入口
-        Jec(nun: 7, name: "7号館", location: CLLocationCoordinate2D(latitude: 35.69890, longitude: 139.69681), aed: true),
-        Jec(nun: 8, name: "8号館", location: CLLocationCoordinate2D(latitude: 35.69761045281154, longitude: 139.69779081005694), aed: true),
-        Jec(nun: 9, name: "9号館", location: CLLocationCoordinate2D(latitude: 35.69938827675634, longitude: 139.69801154259176), aed: true),
+        Jec(nun: 7, name: "7号館", location: CLLocationCoordinate2D(latitude: 35.69888, longitude: 139.69680), aed: true),
+        //入口
+        Jec(nun: 8, name: "8号館", location: CLLocationCoordinate2D(latitude: 35.69745, longitude: 139.69784), aed: true),
+        //メディアホール入口
+        Jec(nun: 9, name: "9号館", location: CLLocationCoordinate2D(latitude: 35.69900, longitude: 139.69800), aed: true),
         Jec(nun: 10, name: "10号館", location: CLLocationCoordinate2D(latitude: 35.69955123784045, longitude: 139.6979809137563), aed: false),
         Jec(nun: 11, name: "11号館", location: CLLocationCoordinate2D(latitude: 35.70089900147449, longitude: 139.69795110478069), aed: true),
-        Jec(nun: 12, name: "12号館", location: CLLocationCoordinate2D(latitude: 35.69554665009267, longitude: 139.69899678492044), aed: true)
+        //入口
+        Jec(nun: 12, name: "12号館", location: CLLocationCoordinate2D(latitude: 35.69530, longitude: 139.69900, aed: true)
     ]
     
 }
